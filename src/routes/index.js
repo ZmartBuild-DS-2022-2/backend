ada18571e20fcdcccac8411756a4a7b0391f1115
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-10-01 12:00:14 -0300

    connection working

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@ import { Router } from 'express'
 const router = Router()
 
 /* GET home page. */
-router.get('/ping', (req, res, next) => {
-  res.status(200).json({ message: 'pong' });
-});
+router.get('/ping', (req, res) => {
+  res.status(200).json({ message: 'pong' })
+})
 
 export default router
