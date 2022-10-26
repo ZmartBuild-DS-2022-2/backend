63c143d42b4d66fad8faae675816fec36c010de3
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-10-26 11:54:51 -0300

    connection to RDS pg database working, must test it

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,10 @@ import Sequelize from "sequelize"
 import UserModel from "../models/user.model.js"
 import { DB_USER, DB_NAME, DB_PASSWORD, DB_HOST, DB_PORT } from "./config.js"
 
-const sequelize = new Sequelize(DB_USER, DB_NAME, DB_PASSWORD, {
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
-  dialect: "mysql",
+  dialect: "postgres",
 })
 
 export const User = UserModel(sequelize, Sequelize)
