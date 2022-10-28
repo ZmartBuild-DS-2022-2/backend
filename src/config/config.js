f91359df64c8591429dcf67b9326522eaba19d4e
FeBalla <fballadares@uc.cl>
2022-10-28 15:58:47 -0300

    fix: details

--- src/config/config.js
+++ src/config/config.js
@@ -10,5 +10,5 @@ export const JWT_SECRET = process.env.JWT_SECRET
 export const DB_USER = process.env.DB_USER || ""
 export const DB_NAME = process.env.DB_NAME || ""
 export const DB_PASSWORD = process.env.DB_PASSWORD || ""
-export const DB_HOST = process.env.DB_HOST || "localhost"
+export const DB_HOST = process.env.DB_HOST || "http://localhost:3000"
 export const DB_PORT = process.env.DB_PORT || 3306
