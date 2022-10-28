60f992fa6f12f5561f53e6627fd5c3056695b468
FeBalla <fballadares@uc.cl>
2022-10-28 16:02:26 -0300

    fix: more details

--- src/config/config.js
+++ src/config/config.js
@@ -3,12 +3,12 @@ import { config } from "dotenv"
 // Load environment variables
 config()
 export const PORT = process.env.PORT || 3000
-export const ORIGIN = process.env.ORIGIN || "localhost"
+export const ORIGIN = process.env.ORIGIN || "http://localhost:3000"
 export const JWT_SECRET = process.env.JWT_SECRET
 
 // Database environment and configuration variables
 export const DB_USER = process.env.DB_USER || ""
 export const DB_NAME = process.env.DB_NAME || ""
 export const DB_PASSWORD = process.env.DB_PASSWORD || ""
-export const DB_HOST = process.env.DB_HOST || "http://localhost:3000"
+export const DB_HOST = process.env.DB_HOST || ""
 export const DB_PORT = process.env.DB_PORT || 3306
