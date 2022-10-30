90ec054149c630f32478be7fa564f09a2242a481
NicoMeyer <nmeya@uc.cl>
2022-10-29 22:39:36 -0300

    Changed DDBB from Mysql to Postgres; Only dockerize BBDD

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ import { DB_USER, DB_NAME, DB_PASSWORD, DB_HOST, DB_PORT } from "./config.js"
 const sequelize = new Sequelize(DB_USER, DB_NAME, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
-  dialect: "mysql",
+  dialect: "postgres",
 })
 
 export const User = UserModel(sequelize, Sequelize)
