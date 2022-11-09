ee1f1d1c7dd6f04b9959c79eb96a40eafa27451a
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-11-09 10:18:41 -0300

    fix: config cors origin for develop and added config for connection to dev DB in render app

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ import { config } from "dotenv"
 // Load environment variables
 config()
 export const PORT = process.env.PORT || 3000
-export const ORIGIN = process.env.ORIGIN || "localhost"
+export const ORIGIN = process.env.ORIGIN || "http://localhost:3000"
 export const JWT_SECRET = process.env.JWT_SECRET
 
 // Database environment and configuration variables
@@ -12,3 +12,4 @@ export const DB_NAME = process.env.DB_NAME || ""
 export const DB_PASSWORD = process.env.DB_PASSWORD || ""
 export const DB_HOST = process.env.DB_HOST || "localhost"
 export const DB_PORT = process.env.DB_PORT || 5432
+export const DB_SSL = process.env.DB_SSL == 'true' ? true : false
--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,15 @@
 import Sequelize from "sequelize"
 import UserModel from "../models/user.model.js"
-import { DB_USER, DB_NAME, DB_PASSWORD, DB_HOST, DB_PORT } from "./config.js"
+import { DB_USER, DB_NAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_SSL } from "./config.js"
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
   dialect: "postgres",
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: DB_SSL,
+  },
 })
 
 export const User = UserModel(sequelize, Sequelize)
