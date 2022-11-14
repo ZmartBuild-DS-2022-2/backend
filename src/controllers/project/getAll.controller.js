c603cdf8739148c44f9f2098e7061bef4f28e49e
unknown <jretamalfarias@uc.cl>
2022-11-14 05:59:35 -0300

    feat: added endpoints for getting a single project and all projects a user has access to

--- src/controllers/project/getAll.controller.js
+++ src/controllers/project/getAll.controller.js
@@ -1,24 +1,22 @@
 import { User } from "../../config/db.js"
 
 const getAllController = async (req, res) => {
-  const userProjects = await User.findByPk(req.currentUser.id,
-    { include: "projects" }
-  )
+  const userProjects = await User.findByPk(req.currentUser.id, { include: "projects" })
 
   const projects = []
 
   userProjects.projects.forEach((item) => {
     projects.push({
-      "id": item.id,
-      "name": item.name,
-      "description": item.description,
-      "projectPermission": {
-        "role": item.projectPermission.role
-      }
+      id: item.id,
+      name: item.name,
+      description: item.description,
+      projectPermission: {
+        role: item.projectPermission.role,
+      },
     })
   })
 
-  return res.status(200).json({"projects": projects})
+  return res.status(200).json({ projects: projects })
 }
 
 export default getAllController
--- src/controllers/project/getOne.controller.js
+++ src/controllers/project/getOne.controller.js
@@ -8,11 +8,10 @@ const getOneController = async (req, res) => {
   try {
     const project = await Project.findByPk(id)
 
-    return res.status(200).json({ "project": project })
+    return res.status(200).json({ project: project })
   } catch (err) {
     return res.status(400).send(err.message)
   }
-
 }
 
 export default getOneController
