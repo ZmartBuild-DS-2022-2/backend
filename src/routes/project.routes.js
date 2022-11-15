124f9bb4c746d7b68d96a6a0cbe32a84b3dfd63b
unknown <jretamalfarias@uc.cl>
2022-11-15 20:47:54 -0300

    feat: added endpoint for assigning roles to users in a project

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -3,6 +3,7 @@ import getProjectsController from "../controllers/project/getProjects.controller
 import getProjectController from "../controllers/project/getProject.controller.js"
 import updateProjectController from "../controllers/project/updateProject.controller.js"
 import deleteProjectController from "../controllers/project/deleteProject.controller.js"
+import addUserController from "../controllers/project/addUser.controller.js"
 import verifyReadProjectPermmission from "../middlewares/project/readProject.js"
 import verifyWriteProjectPermmission from "../middlewares/project/writeProject.js"
 import verifyAdminProjectPermmission from "../middlewares/project/adminProject.js"
@@ -16,5 +17,6 @@ router.get("/getAll", [verifyToken], getProjectsController)
 router.get("/:projectId", [verifyToken, verifyReadProjectPermmission], getProjectController)
 router.patch("/:projectId", [verifyToken, verifyWriteProjectPermmission], updateProjectController)
 router.delete("/:projectId", [verifyToken, verifyAdminProjectPermmission], deleteProjectController)
+router.post("/:projectId/addUser", [verifyToken, verifyAdminProjectPermmission], addUserController)
 
 export default router
