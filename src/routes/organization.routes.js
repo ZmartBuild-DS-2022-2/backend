7f980ec5e9493f88c9a6e4122f67cc689f1e3631
FeBalla <fballadares@uc.cl>
2022-11-16 20:17:58 -0300

    fix: remove not reviewed routes

--- src/routes/organization.routes.js
+++ src/routes/organization.routes.js
@@ -1,10 +1,8 @@
 import {
   addUserToOrganizationController,
   createOrganizationController,
-  deleteOrganizationController,
   getOrganizationByIdController,
   getUserOrganizationsController,
-  updateOrganizationController,
 } from "../controllers/organization/index.js"
 import { Router } from "express"
 import verifyToken from "../middlewares/auth.js"
@@ -14,21 +12,13 @@ const router = Router()
 
 router.post("/", [verifyToken], createOrganizationController)
 router.get("/", [verifyToken], getUserOrganizationsController)
+
 router.get(
   "/:organizationId",
   [verifyToken, verifyOrganizationPermission],
   getOrganizationByIdController
 )
-router.patch(
-  "/:organizationId",
-  [verifyToken, verifyOrganizationPermission],
-  updateOrganizationController
-)
-router.delete(
-  "/:organizationId",
-  [verifyToken, verifyOrganizationPermission],
-  deleteOrganizationController
-)
+
 router.post(
   "/:organizationId/user",
   [verifyToken, verifyOrganizationPermission],
