22016afa514d30bd3502233279d3ec35d2549551
FeBalla <fballadares@uc.cl>
2022-11-16 23:33:31 -0300

    fix: permission when creating project

--- src/controllers/project/createProject.controller.js
+++ src/controllers/project/createProject.controller.js
@@ -6,12 +6,14 @@ const createProjectController = async (req, res) => {
   if (!name) {
     return res.status(400).send("You must complete all required fields")
   }
-
+  
+  const user = req.currentUser
   const organization = await Organization.findByPk(organizationId)
   const project = Project.build(req.body)
   try {
     const newProject = await project.save({ fields: ["name", "description", "imgUrl"] })
     await organization.addProject(newProject)
+    await user.addProject(newProject, { through: { role: "a" } })
 
     return res.status(201).json({
       id: newProject.id,
