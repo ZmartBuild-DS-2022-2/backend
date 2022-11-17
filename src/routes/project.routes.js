d55796de14db49c77e00b3f6befb514abab2d2cb
FeBalla <fballadares@uc.cl>
2022-11-17 02:22:31 -0300

    fix: disable not implemented endpoints

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,9 +1,4 @@
-import updateProjectController from "../controllers/project/updateProject.controller.js"
-import deleteProjectController from "../controllers/project/deleteProject.controller.js"
-import addUserController from "../controllers/project/addUser.controller.js"
 import verifyReadProjectPermmission from "../middlewares/project/readProject.js"
-import verifyWriteProjectPermmission from "../middlewares/project/writeProject.js"
-import verifyAdminProjectPermmission from "../middlewares/project/adminProject.js"
 import verifyToken from "../middlewares/auth.js"
 import { Router } from "express"
 import {
@@ -15,8 +10,5 @@ const router = Router()
 
 router.get("/", [verifyToken], getUserProjectsController)
 router.get("/:projectId", [verifyToken, verifyReadProjectPermmission], getProjectByIdController)
-router.patch("/:projectId", [verifyToken, verifyWriteProjectPermmission], updateProjectController)
-router.delete("/:projectId", [verifyToken, verifyAdminProjectPermmission], deleteProjectController)
-router.post("/:projectId/addUser", [verifyToken, verifyAdminProjectPermmission], addUserController)
 
 export default router
