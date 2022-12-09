ce2f673f9f99183d41e31fa50c9bfdc2dab7934d
NicoMeyer <nmeya@uc.cl>
2022-12-09 15:07:48 -0300

    Made changes so the front can get the role of the user when rendering an organization/project/subproject

--- src/controllers/project/getProjectById.controller.js
+++ src/controllers/project/getProjectById.controller.js
@@ -1,11 +1,13 @@
-import { Organization, Project, ProjectImage } from "../../config/db.js"
+import { Organization, ProjectImage } from "../../config/db.js"
 
 const getProjectByIdController = async (req, res) => {
+  const { currentUser } = req
   const { projectId } = req.params
   try {
-    const project = await Project.findByPk(projectId, {
+    const project = await currentUser.getUserProjects({
       attributes: ["id", "name", "description"],
-      joinTableAttributes: [],
+      where: { id: projectId },
+      joinTableAttributes: ["role"],
       include: [
         {
           model: Organization,
@@ -17,7 +19,8 @@ const getProjectByIdController = async (req, res) => {
         },
       ],
     })
-    return res.status(200).json(project)
+    if (project.length > 0) return res.status(200).json(project[0])
+    return res.status(404).send(`Couldn't find project with id ${projectId}`)
   } catch (err) {
     try {
       return res.status(400).send(err.errors[0]?.message)
