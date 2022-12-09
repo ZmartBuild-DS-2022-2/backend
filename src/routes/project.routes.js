781a22cd6486d97dab455e9be1e545cd39f268d2
NicoMeyer <nmeya@uc.cl>
2022-12-08 22:09:39 -0300

    Permission in routing from project/subprojects

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -7,15 +7,22 @@ import {
   deleteProjectController,
   addUserToProjectController,
 } from "../controllers/project/index.js"
+import verifyWriteOrganizationPermission from "../middlewares/organization/writeOrganization.js"
+import verifyWriteProjectPermission from "../middlewares/project/writeProject.js"
+import verifyReadProjectPermission from "../middlewares/project/readProject.js"
 
 const router = Router()
 
-router.post("/:organizationId", [verifyToken], createProjectController)
+router.post(
+  "/:organizationId",
+  [verifyToken, verifyWriteOrganizationPermission],
+  createProjectController
+)
 router.get("/", [verifyToken], getUserProjectsController)
 
-router.get("/:projectId", [verifyToken], getProjectByIdController)
+router.get("/:projectId", [verifyToken, verifyReadProjectPermission], getProjectByIdController)
 
-router.delete("/:projectId", [verifyToken], deleteProjectController)
+router.delete("/:projectId", [verifyToken, verifyWriteProjectPermission], deleteProjectController)
 
 router.post("/:projectId/user", [verifyToken], addUserToProjectController)
 
--- src/routes/subproject.routes.js
+++ src/routes/subproject.routes.js
@@ -5,16 +5,27 @@ import {
   createSubprojectController,
   getSubprojectByIdController,
   getUserSubprojectsController,
+  deleteSubprojectController,
 } from "../controllers/subproject/index.js"
+import verifyWriteProjectPermission from "../middlewares/project/writeProject.js"
+import verifyReadProjectPermission from "../middlewares/project/readProject.js"
 
 const router = Router()
 
-router.post("/:projectId", [verifyToken], createSubprojectController)
+router.post("/:projectId", [verifyToken, verifyWriteProjectPermission], createSubprojectController)
 
 router.get("/", [verifyToken], getUserSubprojectsController)
 
-router.get("/:subprojectId", [verifyToken], getSubprojectByIdController)
+router.get(
+  "/:subprojectId",
+  [verifyToken, verifyReadProjectPermission],
+  getSubprojectByIdController
+)
 
-router.delete("/:subprojectId", [verifyToken])
+router.delete(
+  "/:subprojectId",
+  [verifyToken, verifyWriteProjectPermission],
+  deleteSubprojectController
+)
 
 export default router
