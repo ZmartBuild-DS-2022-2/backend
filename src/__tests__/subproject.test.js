9c862e37fc7b910084746d0d5a20bf4d1eeccd5a
Melisa96 <yerodriguez@uc.cl>
2022-12-11 17:25:02 -0300

    subprojects testing done

--- src/__tests__/subproject.test.js
+++ src/__tests__/subproject.test.js
@@ -9,6 +9,8 @@ describe("Project API routes", () => {
   let subprojectId
   let organizationId
   let token
+  let newToken
+  let invitationResponseId
   let testServer
 
   beforeAll(async () => {
@@ -27,6 +29,12 @@ describe("Project API routes", () => {
     password: "web1234",
   }
 
+  const newUserData = {
+    email: "orlando@gmail.com",
+    fullname: "Orli",
+    lastName: "West",
+    password: "orl123",
+  }
   //   const userData2 = {
   //     email: "orlando@gmail.com",
   //     fullname: "Orli",
@@ -60,13 +68,13 @@ describe("Project API routes", () => {
     bin_file: {},
   }
 
-  // const subprojectDataUpdated = {
-  //   title: "ubproject updated",
-  //   description: "subproject descripction updated",
-  //   images: {},
-  //   gltf_file: {},
-  //   bin_file: {},
-  // }
+  const subprojectDataUpdated = {
+    title: "ubproject updated",
+    description: "subproject descripction updated",
+    images: {},
+    gltf_file: {},
+    bin_file: {},
+  }
 
   const newSubprojectData = {
     title: "title subproyecto2",
@@ -79,8 +87,18 @@ describe("Project API routes", () => {
   const createAuth = (body) => request(app).post("/api/auth/register").send(body)
   const loginAuth = (body) => request(app).post("/api/auth/login").send(body)
 
+  // Organization
+
   const createOrganization = (body, accessToken) =>
     request(app).post("/api/organizations").send(body).set("Cookie", accessToken)
+
+  // Invitation
+
+  const inviteUserToProject = (body, accessToken) =>
+    request(app).post(`/api/invitations`).send(body).set("Cookie", accessToken)
+
+  const respondUserInvitation = (body, invitationId, accessToken) =>
+    request(app).patch(`/api/invitations/${invitationId}`).send(body).set("Cookie", accessToken)
 
   // Projects
   const createProject = (body, orgId, accessToken) =>
@@ -102,8 +120,8 @@ describe("Project API routes", () => {
   const getUserSubprojects = (accessToken) =>
     request(app).get("/api/subprojects").set("Cookie", accessToken)
 
-  // const updateSubproject = (body, subprojId, accessToken) =>
-  // request(app).patch(`/api/subprojects/${subprojId}`).send(body).set("Cookie", accessToken)
+  const updateSubproject = (body, subprojId, accessToken) =>
+  request(app).patch(`/api/subprojects/${subprojId}`).send(body).set("Cookie", accessToken)
 
   beforeAll(async () => {
     // Create and login user
@@ -145,10 +163,11 @@ describe("Project API routes", () => {
     expect(response.status).toBe(200)
   })
 
-  //  test("Should be able to update subproject, expect 200 status code", async () => {
-  //   response = await updateSubproject()
+   test("Should be able to update subproject, expect 200 status code", async () => {
+    response = await updateSubproject(subprojectDataUpdated,subprojectId, token)
+    expect(response.status).toBe(200)
 
-  // })
+  })
 
   test("should delete subproject ", async () => {
     // Create new subproject
@@ -161,5 +180,105 @@ describe("Project API routes", () => {
     expect(await Subproject.count()).toBe(1)
   })
 
-  // Remains permission testing
+  // Permission testing
+  
+  describe("When involves another user authorization", () => {
+    
+    beforeAll(async () => {
+      // Created another user Permission
+      const { email, password } = newUserData
+      await createAuth(newUserData)
+
+      const newLoginResponse = await loginAuth({ email, password })
+      newToken = await newLoginResponse.get("set-cookie")[0]
+    })
+
+    //BEFORE INVITATION ACCESS
+
+    test("Should not see the subproject created from another user", async () => {
+      response = await getSubprojectById(subprojectId, newToken)
+      expect(response.status).toBe(401)
+    })
+
+    test("Should not edit the subproject created from another user", async () => {
+      response = await updateSubproject(subprojectDataUpdated,subprojectId, newToken)
+      expect(response.status).toBe(401)
+    })
+
+    test("Should not delete the subproject created from another user", async () => {
+      response = await deleteSubproject(subprojectId, newToken)
+      expect(response.status).toBe(401)
+    })
+
+    //AFTER INVITATION ACCESS
+
+    describe("Invite user to project", () => {
+
+        // Create an invitation to the new user
+        // AccessType is irrelevant here
+
+        test("User received an invitation but it isn't accepted yet", async () => {
+          const invitationData = {
+            objectiveId: projectId,
+            email: newUserData.email,
+            type: "project",
+            accessType: "a",
+          }
+          const { objectiveId, email, type, accessType } = invitationData
+          const invitationResponse = await inviteUserToProject(
+            { objectiveId, email, type, accessType },
+            token
+          )
+          invitationResponseId = await invitationResponse.body.id
+  
+          response = await getSubprojectById(subprojectId, newToken)
+          expect(response.status).toBe(401)
+        })
+
+        test("User accepted the invitation & should be able to get user subprojects,\
+         expect 200 status code", async () => {
+          // We simulate the new user accepting the invitation
+          const updateInvitationData = {
+            id: invitationResponseId,
+            accessType: "a",
+            state: "Accepted",
+          }
+          await respondUserInvitation(updateInvitationData, updateInvitationData.id, newToken)
+        
+          response = await getSubprojectById(subprojectId, newToken)
+          expect(response.status).toBe(200)
+        })
+
+        test("User accepted the invitation & should be able to edit user subprojects,\
+        expect 200 status code", async () => {
+         // We simulate the new user accepting the invitation
+         const updateInvitationData = {
+           id: invitationResponseId,
+           accessType: "a",
+           state: "Accepted",
+         }
+         await respondUserInvitation(updateInvitationData, updateInvitationData.id, newToken)
+       
+         response = await updateSubproject(subprojectDataUpdated, subprojectId, newToken)
+         expect(response.status).toBe(200)
+       })
+
+       test("User accepted the invitation & should be able to delete user subprojects,\
+       expect 200 status code", async () => {
+        // We simulate the new user accepting the invitation
+        const updateInvitationData = {
+          id: invitationResponseId,
+          accessType: "a",
+          state: "Accepted",
+        }
+        await respondUserInvitation(updateInvitationData, updateInvitationData.id, newToken)
+      
+        response = await deleteSubproject(subprojectId, newToken)
+        expect(response.status).toBe(200)
+      })
+      })
+  })
 })
+
+
+
